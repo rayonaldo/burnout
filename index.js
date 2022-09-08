72f7a523b90761865d15dffba0e31e4b35b425ea
ray omego <ray.omego@student.moringaschool.com>
2022-09-08 17:00:14 +0300

    added js

--- index.js
+++ index.js
@@ -36,3 +36,11 @@ var swiper = new Swiper(".home-slider", {
     },
     
   });
+
+  const header =  document.querySelector('.header');
+  window.addEventListener('scroll', ()=>{
+    if(window.scrollY >= 70){
+        header.classList.add('header-shadow')
+    }
+    else header.classList.remove('header-shadow')
+  })
